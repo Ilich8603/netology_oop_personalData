4f187439232f68150b5ff8b63f671670e6cc7ac1
Ilich8603 <dremin86ilja@gmail.com>
2025-06-18 16:18:54 +0600

    Initial commit

---
+++ src/main/java/ru/netology/oop/FormDate.java
@@ -0,0 +1,8 @@
+package ru.netology.oop;
+
+public class FormDate {
+
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/oop/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.oop;
+
+public class Post {
+
+    public FormDate birthday;
+
+    public String name;
+    public String passport = "4444 № 44444444";
+    public String patronymic = "Иванович";
+    public String phone = "+7 (999)-999-99-99";
+    public String surname = "Иванов";
+    public boolean subscription;
+
+}
